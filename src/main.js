ef295d5e16531b2797637ab3c35e3753e1860684
wet <2467530813@qq.com>
2022-07-03 11:57:28 +0800

    做出来了一些基础的配置

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import "@/styles/index.less";
 Vue.config.productionTip = false;
-
+import Vant from "vant";
+import "vant/lib/index.css";
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
+
+Vue.use(Vant);
